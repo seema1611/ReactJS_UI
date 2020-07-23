52026e6b47ec848d60fd1c83cda39d1681ae595b
seema1611 <seemarajpure16@gmail.com>
2020-07-23 15:51:07 +0700

    ADDED: feature add header button

---
+++ react-ui/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+import Header from './Header.css';
+
+class Navbar extends Component {
+
+    state={
+        isOpen: false
+    }
+
+    handleClick=()=>{
+        this.setState({
+            isOpen:!this.state.isOpen,
+        })
+    }
+
+
+    render() {
+        return (
+        <nav>
+            <div className="logoBtn">
+                <div className="logo">
+                <p>Company name</p>
+                </div>
+        
+                <div className="btn" onClick=
+                {this.handleClick}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+
+                </div>
+            </div>
+            <ul className={this.state.isOpen ? 
+            'showNav':'undefined'}>
+              <li><a href="#">FEATURE</a></li>
+              <li><a href="#">ENTERPRISES</a></li>
+              <li><a href="#">SUPPORT</a></li>
+              <li><a href="#">LOGIN</a></li>
+            </ul>
+        
+        </nav>
+        )
+    }
+}
+
+
+export default Navbar;
