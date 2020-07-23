e7d4372ba62bf782872868aa79d83241b1834f18
seema1611 <seemarajpure16@gmail.com>
2020-07-23 23:40:24 +0700

    ADDED: add subscript tag for /mo

--- react-ui/src/components/PriceTypes.js
+++ react-ui/src/components/PriceTypes.js
@@ -10,7 +10,7 @@ export default class PriceTypes extends Component {
                         <p>Free</p>
                     </div>
 
-                    <p><h2>$0 /mo</h2>
+                    <p><h2>$0<sub>/mo</sub></h2>
           10 users included</p><p> 2 GB of storage</p><p> Help center access </p>
                     <p>Email support</p>
                     <button class="button-color" type="submit">SIGN UP FOR FREE</button>
@@ -22,7 +22,7 @@ export default class PriceTypes extends Component {
                         <h4>Most popular</h4>
                     </div>
 
-                    <h2>$15 /mo</h2>
+                    <h2>$15<sub>/mo</sub></h2>
                     <p> 20 users included</p><p>10 GB of storage</p>
                     <p>Help center access </p>
                     <p>Email support</p>
@@ -33,7 +33,7 @@ export default class PriceTypes extends Component {
                     <div className="free">
                         <p>Enterprise</p>
                     </div>
-                    <h2>$30 /mo</h2>
+                    <h2>$30 <sub>/mo</sub></h2>
                     <p> 50 users included</p><p>30 GB of storage</p><p> Help center access</p><p>Phone and Email support</p>
                     <button class="button-color" type="submit">CONTACT US</button>
                 </div>
